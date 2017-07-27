494973792eb341950e888322fdd111bfa5494697
Albert <lohoi@umich.edu>
2017-07-27 00:34:05 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+	var webApp = angular.module('webApp',[]);
+
+	// webApp.config(function($stateProvider) {
+	//   var home = {
+	//     name: 'home',
+	//     url: '/home',
+	//     template: '<h3>hello world!</h3>'
+	//   }
+
+	//   $stateProvider.state(home);
+	// });
+})
