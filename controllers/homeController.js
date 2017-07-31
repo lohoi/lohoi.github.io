77952c53d8b406638e9ea10d4c0828e9f9338f4c
Albert <lohoi@umich.edu>
2017-07-31 00:09:44 -0600

    flex

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,6 +2,6 @@ app.controller('HomeCtrl', function($scope) {
     $scope.myInterval = 5000;
     $scope.slides = [{
         "id": 0,
-        "image": "img/lawquadmichigan3.jpg"
+        "image": "img/rickandmorty.jpg"
     }]
 });
